1048daf92f01749238947ef525bfcef0f5d12541
tompng <tomoyapenguin@gmail.com>
2023-08-23 02:01:14 +0900

    implement workaround for missing token ignored by ripper

--- lib/indent_calculator_demo.rb
+++ lib/indent_calculator_demo.rb
@@ -3,9 +3,27 @@ require_relative 'indent_calculator_demo/nesting_parser'
 
 module IndentCalculatorDemo
   def self.tokenize(code)
-    Ripper::Lexer.new(code).scan.chunk(&:pos).map do |_pos, same_pos_tokens|
+    tokens = Ripper::Lexer.new(code).scan.chunk(&:pos).map do |_pos, same_pos_tokens|
       same_pos_tokens.find { !_1.event.end_with?('_error') } || same_pos_tokens.first
     end
+    interpolate_missing_tokens code, tokens
+  end
+
+  # Workaround for https://bugs.ruby-lang.org/issues/19736 and other similar behavior
+  def self.interpolate_missing_tokens(code, tokens)
+    bytepos_by_line = [0]
+    code.lines.each { bytepos_by_line << bytepos_by_line.last + _1.bytesize }
+    prev_token_end_pos = 0
+    interpolated = []
+    tokens.each do |token|
+      token_start_pos = bytepos_by_line[token.pos[0] - 1] + token.pos[1]
+      if prev_token_end_pos < token_start_pos
+        interpolated << Ripper::Lexer::Elem.new([0, 0], :on_ignored_by_ripper, code.byteslice(prev_token_end_pos...token_start_pos), 0)
+      end
+      interpolated << token
+      prev_token_end_pos = token_start_pos + token.tok.bytesize
+    end
+    interpolated
   end
 
   def self.calculate(code)
@@ -24,7 +42,7 @@ module IndentCalculatorDemo
       when :on_embdoc_beg
         indent_level = 0
       when :on_op
-        # ignore. `a ? b : c` and `do |param|`
+        # ignore nesting of `a ? b : c` and `|block_parameter|`
       else
         indent_level += 1
       end
